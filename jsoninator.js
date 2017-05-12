a8374d72c6ebd923a13c90e041d4363fc9c69982
Brian Wong <brianwong.se@gmail.com>
2017-05-12 16:35:59 -0700

    More stringify cleanup

--- jsoninator.js
+++ jsoninator.js
@@ -10,40 +10,13 @@ const stringify = function(obj) {
       return '"' + obj + '"';
     case 'object':
       if (obj === null) return 'null';
-      if (Array.isArray(obj)) {
-        return '[' + obj.map(el => stringify(el)) + ']';
-      }
-      return '{' + Object.keys(obj).map(key => stringify(key) + ':' + stringify(obj[key])) + '}';
+      return (Array.isArray(obj))
+        ? '[' + obj.map(el => stringify(el)) + ']'
+        : '{' + Object.keys(obj).map(key => stringify(key) + ':' + stringify(obj[key])) + '}';
     default:
       return String(obj);
   }
 };
-// const stringify = function(obj) {
-//   let res = '';
-//   if (typeof obj === 'string') {
-//     res += '"' + obj + '"';
-//   } else if (typeof obj !== 'object' || obj === null) {
-//     res += String(obj);
-//   } else if (Array.isArray(obj)) {
-//     res += '[';
-//     if (obj.length) {
-//       for (el of obj) {
-//         res += stringify(el) + ',';
-//       }
-//       res = res.substring(0, res.length - 1);
-//     }
-//     res += ']';
-//   } else if (typeof obj === 'object') {
-//     res += '{';
-//     if (Object.keys(obj).length) {
-//       for (key in obj) {
-//         res += stringify(key) + ':' + stringify(obj[key]);
-//       }
-//     }
-//     res += '}';
-//   }
-//   return res;
-// };
 
 module.exports = {
   stringify: stringify
